d456fd392f750a258e864514657b9032ebe07921
leo-tavares <leonardotavaresdev@gmail.com>
2020-05-10 02:12:16 -0300

    implementação dos endpoints do desafio02 - Conceitos de nodeJs
    A ideia é trabalhar com os fundamentos do express e construir um CRUD que permite manter repositorios de projetos.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,80 @@ app.use(cors());
 
 const repositories = [];
 
+const chkIfRepositoryExists = (request, response, next) => {
+  const { id } = request.params;
+
+  const repoIdx = repositories.findIndex((repo) => repo.id === id);
+  if (repoIdx < 0) {
+    return response.status(400).json({ message: "repository does not exists" });
+  }
+  request.repoIdx = repoIdx;
+  return next();
+};
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, techs, url } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    techs,
+    url,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", chkIfRepositoryExists, (request, response) => {
+  const {
+    body: { title, url, techs },
+    repoIdx,
+  } = request;
+
+  const repository = {
+    ...repositories[repoIdx],
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repoIdx] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", chkIfRepositoryExists, (request, response) => {
+  const {
+    params: { id },
+    repoIdx,
+  } = request;
+
+  repositories.splice(repoIdx, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+app.post(
+  "/repositories/:id/like",
+  chkIfRepositoryExists,
+  (request, response) => {
+    const {
+      params: { id },
+      repoIdx,
+    } = request;
+
+    const auxRepo = repositories[repoIdx];
+    auxRepo.likes = auxRepo.likes + 1;
+
+    return response.json(auxRepo);
+  }
+);
 
 module.exports = app;
